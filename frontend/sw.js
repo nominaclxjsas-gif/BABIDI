0ce31dbd0ce0a885ec3b53a423b0f7fc0db6c041
esteban835 <danis.torres-835970@outlook.com>
2026-05-09 10:03:44 -0500

    feat: add PWA manifest and service worker

---
+++ frontend/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'babidi-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/favicon.svg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});

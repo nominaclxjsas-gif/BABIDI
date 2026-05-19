c3c30b75e8c3476e580ed6b1273660862c67ff9e
esteban835 <danis.torres-835970@outlook.com>
2026-05-19 10:04:59 -0500

    Descripción de los cambios

--- frontend/sw.js
+++ frontend/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'babidi-v1';
+const CACHE_NAME = 'babidi-v3';
 const ASSETS = [
   '/',
   '/index.html',
@@ -7,9 +7,19 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', e => {
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
